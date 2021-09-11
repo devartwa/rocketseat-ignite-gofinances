f108b4a546cd0250f0c8294c62f32fa68c0ab454
Arthur Bueno <arthurbueno@MacBook-Air-de-Arthur.local>
2021-09-11 19:56:46 -0300

    go finances dashboard completed - ignite

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { HighlightCard } from '../../components/HighlightCard';
+import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard, TransactionCardProps } from "../../components/TransactionCard";
 import {
     Container,
     Header,
@@ -11,15 +12,61 @@ import {
     UserName,
     Icon,
     HighlightCards,
-} from './styles';
+    Transactions,
+    Title,
+    TransactionsList
+} from "./styles";
+
+export interface DataListProps extends TransactionCardProps {
+    id: string;
+}
 
 export function Dashboard() {
+    const values: DataListProps[] = [
+        {
+            id: '1',
+            type: 'positive',
+            title: "Desenvolvimento de site",
+            amount: "R$ 15.000,00",
+            category: {
+                name: "Vendas",
+                icon: "dollar-sign",
+            },
+            date: "16/09/1994",
+        },
+        {
+            id: '2',
+            type: 'negative',
+            title: "Pizza",
+            amount: "R$ 59,00",
+            category: {
+                name: "Alimentação",
+                icon: "coffee",
+            },
+            date: "16/09/1994",
+        },
+        {
+            id: '3',
+            type: 'negative',
+            title: "Aluguel",
+            amount: "R$ 1.000,00",
+            category: {
+                name: "Casa",
+                icon: "home",
+            },
+            date: "16/09/1994",
+        },
+    ];
     return (
         <Container>
             <Header>
                 <UserWrapper>
                     <UserInfo>
-                        <Photo source={{ uri: "https://avatars.githubusercontent.com/u/49029304?v=4" }} />
+                        <Photo
+                            source={{
+                                uri: "https://avatars.githubusercontent.com/u/49029304?v=4",
+                            }}
+                        />
                         <User>
                             <UserGreeting>Olá,</UserGreeting>
                             <UserName>Arthur</UserName>
@@ -49,6 +96,15 @@ export function Dashboard() {
                     lastTransaction="Última saída dia 11 de abril"
                 />
             </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+                <TransactionsList
+                    data={values}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                />
+            </Transactions>
         </Container>
     );
-};
+}
