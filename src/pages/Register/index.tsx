d47ea92d045646b6cd6be4c0e5dd60d1d9d85382
Arthur Bueno <arthurbueno@MacBook-Air-de-Arthur.local>
2021-09-12 09:15:48 -0300

    go finances register - ignite

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Input } from '../../components/Form/Input';
+import { Button } from '../../components/Form/Button';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
+
+import {
+    Container,
+    Header,
+    Title,
+    Form,
+    Fields,
+    TransactionsType,
+} from './styles';
+
+export function Register() {
+
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionsTypeSelect(type: 'up' | 'down') {
+        setTransactionType(type);
+    }
+
+    return (
+        <Container>
+            <Header>
+                <Title>Cadastro</Title>
+            </Header>
+
+            <Form>
+                <Fields>
+                    <Input
+                        placeholder="Nome"
+                    />
+
+                    <Input
+                        placeholder="Preço"
+                    />
+
+                    <TransactionsType>
+                        <TransactionTypeButton
+                            type="up"
+                            title="Income"
+                            onPress={() => handleTransactionsTypeSelect('up')}
+                            isActive={transactionType === "up"}
+                        />
+
+                        <TransactionTypeButton
+                            type="down"
+                            title="Outcome"
+                            onPress={() => handleTransactionsTypeSelect('down')}
+                            isActive={transactionType === "down"}
+                        />
+                    </TransactionsType>
+
+                    <CategorySelectButton title="Categoria" />
+                </Fields>
+
+                <Button title="Enviar" />
+            </Form>
+        </Container>
+    );
+};
